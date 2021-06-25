5c21878b8574f90448dcf8a25e9b0187613166fb
Robert Fry <robert.e.fry@protonmail.com>
2021-06-25 20:34:37 +0100

    Query state of `KEY_W` with `KEY_Q`.

--- Sandbox/src/Sandbox.cc
+++ Sandbox/src/Sandbox.cc
@@ -22,6 +22,26 @@ Sandbox::Sandbox()
     : m_Window{GetInitialWindowDetails()}
     , m_Keyboard{}
 {
+    using namespace RockHopper;
+
+    m_Keyboard.persist_event_listener(EventFunctionListener<KeyEvent,KeyPressEvent>{[](KeyPressEvent const& event)
+    {
+        if (event.key == KeyCode::KEY_SPACE)
+        {
+            float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            float g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            float b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            glClearColor(r,g,b,1);
+        }
+    }});
+    m_Keyboard.persist_event_listener(EventFunctionListener<KeyEvent,KeyPressEvent>{[](KeyPressEvent const& event)
+    {
+        if (event.key == KeyCode::KEY_Q)
+        {
+            ROCKHOPPER_LOG_DEBUG("Querying the state of KEY_W - {}.",(event.keyboard->key(KeyCode::KEY_W).down() ? "down" : "up"));
+        }
+    }});
+
     m_Window.attach(&m_Keyboard);
     m_Window.start();
 }
@@ -33,18 +53,6 @@ Sandbox::~Sandbox()
 void Sandbox::run()
 {
     using namespace RockHopper;
-
-    auto background_changer = EventFunctionListener<KeyEvent,KeyReleaseEvent>([](KeyReleaseEvent const& event)
-    {
-        if (event.key == KeyCode::KEY_SPACE)
-        {
-            float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-            float g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-            float b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-            glClearColor(r,g,b,1);
-        }
-    });
-    m_Window.keyboard()->KeyEventHandler::insert_event_listener(&background_changer);
 
     EventWaitListener<EngineTerminationEvent> termination_event_listener;
     m_Window.Engine::insert_event_listener(&termination_event_listener);
