b35ecae8638fe095b29064bfec8d791788b6ec26
Robert E Fry <robert.e.fry@protonmail.com>
2022-04-04 17:36:16 +0100

    rockhopper: move main function to client

--- RockHopper/tests/src/main.cc
+++ RockHopper/tests/src/main.cc
@@ -1,32 +1,32 @@
 
 #include <gtest/gtest.h>
 
-#include "RockHopper/RockHopper.hh"
+// #include "RockHopper/RockHopper.hh"
 
-class TestApp : public RockHopper::Application
-{
-public:
-    virtual ~TestApp() = default;
-    explicit TestApp() = default;
+// class TestApp : public RockHopper::Application
+// {
+// public:
+//     virtual ~TestApp() = default;
+//     explicit TestApp() = default;
 
-    virtual int run() override;
-};
+//     virtual int run() override;
+// };
 
-int TestApp::run()
-{
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
-}
+// int TestApp::run()
+// {
+//     testing::InitGoogleTest();
+//     return RUN_ALL_TESTS();
+// }
 
-/*
- * main function implemented in RockHopper.cpp
- */
+// /*
+//  * main function implemented in RockHopper.cpp
+//  */
 
-// Required by RockHopper engine
-RockHopper::Application* RockHopper::CreateApplication()
-{
-    return new TestApp{};
-}
+// // Required by RockHopper engine
+// RockHopper::Application* RockHopper::CreateApplication()
+// {
+//     return new TestApp{};
+// }
 
 TEST(MAIN,TESTING)
 {
