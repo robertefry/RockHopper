cc5382864f57ae45b279bae30f1835fed1e55fcc
Robert Fry <inbox@robertfry.xyz>
2022-09-10 14:58:47 +0100

    Initial Overhaul Commit     - move all RockHopper files to a backup directory

--- RockHopper/tests/src/main.cc
+++ RockHopper/tests/src/main.cc
@@ -1,33 +1,6 @@
 
 #include <gtest/gtest.h>
 
-// #include "RockHopper/RockHopper.hh"
-
-// class TestApp : public RockHopper::Application
-// {
-// public:
-//     virtual ~TestApp() = default;
-//     explicit TestApp() = default;
-
-//     virtual int run() override;
-// };
-
-// int TestApp::run()
-// {
-//     testing::InitGoogleTest();
-//     return RUN_ALL_TESTS();
-// }
-
-// /*
-//  * main function implemented in RockHopper.cpp
-//  */
-
-// // Required by RockHopper engine
-// RockHopper::Application* RockHopper::CreateApplication()
-// {
-//     return new TestApp{};
-// }
-
 TEST(MAIN,TESTING)
 {
     std::cout << "GTest tests are working :)" << "\n";
