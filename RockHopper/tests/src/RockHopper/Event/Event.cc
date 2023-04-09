41b4c4595769a7572e170d20c2dc894a7773911b
Robert Fry <inbox@robertfry.xyz>
2023-04-09 23:13:59 +0100

    event: refactor tests

--- RockHopper/tests/src/RockHopper/Event/Event.cc
+++ RockHopper/tests/src/RockHopper/Event/Event.cc
@@ -7,112 +7,84 @@
 #include <unordered_map>
 #include <type_traits>
 
-enum ArgumentQualifier
-{
-    VAL, L_REF, CL_REF, R_REF, CR_REF,
-};
-
 /* ************************************************************************** */
 
 struct TestEvent1 { std::unique_ptr<int> no_copy; };
 struct TestEvent2 { std::unique_ptr<int> no_copy; };
 using TestEvent = RockHopper::Event::EventSet<TestEvent1,TestEvent2>;
 
-template <typename T_Event>
-struct TestEvent_Listener : TestEvent::Listener
-{
-    std::unordered_map<ArgumentQualifier,size_t> counts {};
-
-    void on_event(T_Event const&) override { counts[CL_REF] += 1; }
-    void on_event(T_Event &) override { counts[L_REF] += 1; }
-    void on_event(T_Event const&&) override { counts[CR_REF] += 1; }
-    void on_event(T_Event &&) override { counts[R_REF] += 1; }
-
-    using TestEvent::Listener::on_event;
-};
-
 /* ************************************************************************** */
 
 TEST_CASE("EventSet::Dispatch correctly dispatches an event to a listener")
 {
+    struct Listener : TestEvent::Listener
+    {
+        enum ArgumentQualifier
+        {
+            VAL, L_REF, CL_REF, R_REF, CR_REF,
+        };
+        std::unordered_map<ArgumentQualifier,size_t> counts {};
+
+        using TestEvent::Listener::on_event;
+        void on_event(TestEvent1 const&) override { counts[CL_REF] += 1; }
+        void on_event(TestEvent1 &) override { counts[L_REF] += 1; }
+        void on_event(TestEvent1 const&&) override { counts[CR_REF] += 1; }
+        void on_event(TestEvent1 &&) override { counts[R_REF] += 1; }
+    };
+    Listener listener;
+
+    REQUIRE(listener.counts[Listener::VAL] == 0);
+    REQUIRE(listener.counts[Listener::L_REF] == 0);
+    REQUIRE(listener.counts[Listener::CL_REF] == 0);
+    REQUIRE(listener.counts[Listener::R_REF] == 0);
+    REQUIRE(listener.counts[Listener::CR_REF] == 0);
+
     SECTION("listen for const& events")
     {
-        TestEvent_Listener<TestEvent1> listener;
-
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 0);
-
         TestEvent::Variant const event = TestEvent1{};
         TestEvent::Dispatch(listener,event);
 
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 1);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 0);
+        REQUIRE(listener.counts[Listener::VAL] == 0);
+        REQUIRE(listener.counts[Listener::L_REF] == 0);
+        REQUIRE(listener.counts[Listener::CL_REF] == 1);
+        REQUIRE(listener.counts[Listener::R_REF] == 0);
+        REQUIRE(listener.counts[Listener::CR_REF] == 0);
     }
 
     SECTION("listen for & events")
     {
-        TestEvent_Listener<TestEvent1> listener;
-
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 0);
-
         TestEvent::Variant event = TestEvent1{};
         TestEvent::Dispatch(listener,event);
 
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 1);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 0);
+        REQUIRE(listener.counts[Listener::VAL] == 0);
+        REQUIRE(listener.counts[Listener::L_REF] == 1);
+        REQUIRE(listener.counts[Listener::CL_REF] == 0);
+        REQUIRE(listener.counts[Listener::R_REF] == 0);
+        REQUIRE(listener.counts[Listener::CR_REF] == 0);
     }
 
     SECTION("listen for const&& events")
     {
-        TestEvent_Listener<TestEvent1> listener;
-
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 0);
-
         TestEvent::Variant const event = TestEvent1{};
         TestEvent::Dispatch(listener,std::move(event));
 
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 1);
+        REQUIRE(listener.counts[Listener::VAL] == 0);
+        REQUIRE(listener.counts[Listener::L_REF] == 0);
+        REQUIRE(listener.counts[Listener::CL_REF] == 0);
+        REQUIRE(listener.counts[Listener::R_REF] == 0);
+        REQUIRE(listener.counts[Listener::CR_REF] == 1);
     }
 
     SECTION("listen for const&& events")
     {
-        TestEvent_Listener<TestEvent1> listener;
-
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 0);
-        REQUIRE(listener.counts[CR_REF] == 0);
-
         TestEvent::Variant event = TestEvent1{};
         TestEvent::Dispatch(listener,std::move(event));
 
-        REQUIRE(listener.counts[VAL] == 0);
-        REQUIRE(listener.counts[L_REF] == 0);
-        REQUIRE(listener.counts[CL_REF] == 0);
-        REQUIRE(listener.counts[R_REF] == 1);
-        REQUIRE(listener.counts[CR_REF] == 0);
+        REQUIRE(listener.counts[Listener::VAL] == 0);
+        REQUIRE(listener.counts[Listener::L_REF] == 0);
+        REQUIRE(listener.counts[Listener::CL_REF] == 0);
+        REQUIRE(listener.counts[Listener::R_REF] == 1);
+        REQUIRE(listener.counts[Listener::CR_REF] == 0);
     }
 }
 
