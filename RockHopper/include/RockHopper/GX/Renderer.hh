b1bb66e4d2f180f78d46e60df4060a80bb980bd0
Robert Fry <inbox@robertfry.xyz>
2023-05-31 00:25:34 +0100

    gx: renderer: rework for pointer-like api

--- RockHopper/include/RockHopper/GX/Renderer.hh
+++ RockHopper/include/RockHopper/GX/Renderer.hh
@@ -19,41 +19,43 @@ namespace RockHopper::GX
      */
     class Renderer
     {
-        struct Thread : private Util::NoMove
+        class Thread : private Util::NoMove
             , public Chrono::TickThread<Event::Dispatch::Sequential>
         {
             using TickThread = Chrono::TickThread<Event::Dispatch::Sequential>;
 
+        public:
             virtual ~Thread();
             explicit Thread();
 
+            template <typename T_Func, typename... T_Args>
+            [[nodiscard]] auto push_task(T_Func&& func, T_Args&&... args)
+            {
+                if (alive_id() == std::this_thread::get_id())
+                {
+                    return m_TaskQueue.execute_task(
+                        std::forward<T_Func>(func), std::forward<T_Args>(args)...
+                    );
+                } else {
+                    return m_TaskQueue.push_task(
+                        std::forward<T_Func>(func), std::forward<T_Args>(args)...
+                    );
+                }
+            }
+
+        private:
             void on_event(Chrono::TickEvent_Init const&);
             void on_event(Chrono::TickEvent_Dispose const&);
             void on_event(Chrono::TickEvent_OnTick const&);
 
+        private:
             static constexpr size_t s_TaskQueueInitialCapacity = 1024;
             Util::TaskQueue m_TaskQueue {s_TaskQueueInitialCapacity};
         };
 
     public:
-
-        /*
-         * push a task to be run on the renderer thread
-         */
-        template <typename T_Func, typename... T_Args>
-        [[nodiscard]] auto push_task(T_Func&& func, T_Args&&... args)
-        {
-            if (m_Thread->alive_id() == std::this_thread::get_id())
-            {
-                return m_Thread->m_TaskQueue.execute_task(
-                    std::forward<T_Func>(func), std::forward<T_Args>(args)...
-                );
-            } else {
-                return m_Thread->m_TaskQueue.push_task(
-                    std::forward<T_Func>(func), std::forward<T_Args>(args)...
-                );
-            }
-        }
+        auto operator->() & -> auto& { return m_Thread; }
+        auto operator->() const& -> auto const& { return m_Thread; }
 
     private:
         Util::Singleton<Thread> m_Thread{};
